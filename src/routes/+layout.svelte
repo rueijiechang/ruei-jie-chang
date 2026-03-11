994b7bbc09306eaf513c9320ed52fd6cb110599e
Rachel Chang <rachelchang@RacheldeMacBook-Air.local>
2026-03-10 23:21:23 -0500

    add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import '../app.css';
   import Nav from '$lib/components/Nav.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit'
 </script>
 
 <Nav />
